1c4ac448b6aed22b68aa244c28ab768c3f09673e
Program <67320703+Program132@users.noreply.github.com>
2022-06-12 20:59:42 +0200

    Ajout de l'event des interactions pour SP

--- src/main/java/fr/program/pranction/init/EventsManager.java
+++ src/main/java/fr/program/pranction/init/EventsManager.java
@@ -3,8 +3,10 @@ package fr.program.pranction.init;
 import fr.program.pranction.Pranction;
 import fr.program.pranction.events.PlayerChated;
 import fr.program.pranction.events.PlayerJoined;
+import fr.program.pranction.events.SPInteraction;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.inventory.InventoryClickEvent;
 import org.bukkit.event.player.AsyncPlayerChatEvent;
 import org.bukkit.event.player.PlayerJoinEvent;
 
@@ -19,9 +21,19 @@ public class EventsManager implements Listener {
     public void PlayerJoin(PlayerJoinEvent event) {
         PlayerJoined.init(event, plugin);
     }
+
+
     @EventHandler
     public void PlayerChat(AsyncPlayerChatEvent event) {
         PlayerChated.init(event, plugin);
     }
 
+
+
+    // SanctionPlayer Configs :
+
+    @EventHandler
+    public void SpInteraction(InventoryClickEvent event) {
+        SPInteraction.init(event, plugin);
+    }
 }
